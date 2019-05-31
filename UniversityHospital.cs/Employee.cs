c990936e6700767b37321c83379484a6782b0de3
Bridget Klingler <bridget.klingler@gmail.com>
2019-05-31 14:42:26 -0400

    Employee base values

--- UniversityHospital.cs/Employee.cs
+++ UniversityHospital.cs/Employee.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace UniversityHospital.cs
 {
-    class Employee
+    public class Employee
     {
+        public string Name { get; set; }
+        public int EmployeeNumber { get; set; }
+        public int Salary { get; set; }
+
+        public Employee(string name, int number, int salary)
+        {
+            Name = name;
+            EmployeeNumber = number;
+            Salary = salary;
+        }
     }
 }
